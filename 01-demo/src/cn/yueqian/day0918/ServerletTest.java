161ea5ac08a7b9c0f6aed03fd18c0ab7f264c73d
dss <1029808311@qq.com>
2019-11-08 16:24:13 +0800

    testddd

---
+++ 01-demo/src/cn/yueqian/day0918/ServerletTest.java
@@ -0,0 +1,46 @@
+package cn.yueqian.day0918;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class ServerletTest implements Servlet {
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("hhh");
+		config.getInitParameter("");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
